2705c6aa99ce65ea8f3d9007e00745fed080585d
zohredk <zohrehdk2000@gmail.com>
2024-10-15 21:16:34 +1100

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout/Layout";
 import Shopping from "./containers/Shopping/Shopping";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Layout>
-          <Shopping />
-        </Layout>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/" exact element={<Shopping />} />
+            </Routes>
+          </Layout>
+        </Router>
       </div>
     );
   }
 }
+
 export default App;
--- src/containers/Shopping/Shopping.js
+++ src/containers/Shopping/Shopping.js
@@ -1,11 +1,11 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import Wrapper from "../../hoc/Wrapper";
 import Controls from "../../components/Controls/Controls";
 import Modal from "../../components/UI/Modal/Modal";
 import Order from "../../components/Order/Order";
 import Loader from "../../components/UI/Loader/Loader";
 import axios from "../../axios-orders";
+import { useNavigate } from "react-router-dom";
 
 const prices = {
   product1: 59,
@@ -14,117 +14,90 @@ const prices = {
   product4: 79,
 };
 
-class Shopping extends React.Component {
-  state = {
-    products: null,
-    totalPrice: 0,
-    purchased: false,
-    loading: false,
-  };
+const Shopping = () => {
+  const [products, setProducts] = useState(null);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [purchased, setPurchased] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(
         "https://react-redux-main-1b4b0-default-rtdb.firebaseio.com/products.json"
       )
       .then((response) => {
-        this.setState({ products: response.data });
+        setProducts(response.data);
       });
-  }
+  }, []);
 
-  addProductHandler = (type) => {
-    const prevCount = this.state.products[type];
+  const addProductHandler = (type) => {
+    const prevCount = products[type];
     const updatedCount = prevCount + 1;
-    const updatedProducts = {
-      ...this.state.products,
-    };
+    const updatedProducts = { ...products };
     updatedProducts[type] = updatedCount;
     const priceAdd = prices[type];
-    const prevPrice = this.state.totalPrice;
-    const newPrice = prevPrice + priceAdd;
-    this.setState({ totalPrice: newPrice, products: updatedProducts });
+    const newPrice = totalPrice + priceAdd;
+    setTotalPrice(newPrice);
+    setProducts(updatedProducts);
   };
-  removeProductHandler = (type) => {
-    const prevCount = this.state.products[type];
+
+  const removeProductHandler = (type) => {
+    const prevCount = products[type];
     const updatedCount = prevCount - 1;
-    const updatedProducts = {
-      ...this.state.products,
-    };
+    const updatedProducts = { ...products };
     updatedProducts[type] = updatedCount;
     const priceSub = prices[type];
-    const prevPrice = this.state.totalPrice;
-    const newPrice = prevPrice - priceSub;
-    this.setState({ totalPrice: newPrice, products: updatedProducts });
+    const newPrice = totalPrice - priceSub;
+    setTotalPrice(newPrice);
+    setProducts(updatedProducts);
   };
 
-  purchasedHandler = () => {
-    this.setState({ purchased: true });
+  const purchasedHandler = () => {
+    setPurchased(true);
   };
 
-  modalCloseHandler = () => {
-    this.setState({ purchased: false });
+  const modalCloseHandler = () => {
+    setPurchased(false);
   };
 
-  purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-
-    const order = {
-      products: this.state.products,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Zohreh",
-        email: "zohredkia@gmail.com",
-      },
-    };
-
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchased: false });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const purchaseContinueHandler = () => {
+    navigate("/checkout");
   };
 
-  render() {
-    let order = null;
+  let order = null;
+  if (loading) {
+    order = <Loader />;
+  }
 
-    if (this.state.loading) {
-      order = <Loader />;
-    }
-
-    let controls = <Loader />;
-
-    if (this.state.products) {
-      controls = (
-        <Controls
-          productAdd={this.addProductHandler}
-          productRemove={this.removeProductHandler}
-          price={this.state.totalPrice}
-          order={this.purchasedHandler}
-        />
-      );
-
-      order = (
-        <Order
-          products={this.state.products}
-          price={this.state.totalPrice}
-          continue={this.purchaseContinueHandler}
-          cancel={this.modalCloseHandler}
-        />
-      );
-    }
-
-    return (
-      <Wrapper>
-        <Modal show={this.state.purchased} modalClose={this.modalCloseHandler}>
-          {order}
-        </Modal>
-        {controls}
-      </Wrapper>
+  let controls = <Loader />;
+  if (products) {
+    controls = (
+      <Controls
+        productAdd={addProductHandler}
+        productRemove={removeProductHandler}
+        price={totalPrice}
+        order={purchasedHandler}
+      />
+    );
+    order = (
+      <Order
+        products={products}
+        price={totalPrice}
+        continue={purchaseContinueHandler}
+        cancel={modalCloseHandler}
+      />
     );
   }
-}
+
+  return (
+    <Wrapper>
+      <Modal show={purchased} modalClose={modalCloseHandler}>
+        {order}
+      </Modal>
+      {controls}
+    </Wrapper>
+  );
+};
 
 export default Shopping;
